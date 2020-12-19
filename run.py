676c341b42470394272d91c83b64a0dae61b2e78
joeriroijenga <roijenga.joeri@gmail.com>
2020-12-19 19:52:23 +0100

    matrix rotation (left) with multiple layers

--- run.py
+++ run.py
@@ -1,45 +1,57 @@
 def printMatrix(matrix):
     print("")
-    print("Matrix:")
     for row in matrix:
-        print("[%i] [%i] [%i]" % (row[0], row[1], row[2]))
+        str = ""
+        for item in row:
+            str += "[%i] " % (item)
+        print(str)
     print("")
         
 
-def rotateMatrix(matrix):
-    print("Matrix length: " + str(len(matrix)))
-    moduloOne = True if len(matrix) % 2 else False
+def rotateMatrixLeft(matrix):
+    layer = 0
+    for length in range(len(matrix) - 1, 1, -1):
+        for startPoint in range(layer, length, 1):
+            save = matrix[startPoint - startPoint + layer][startPoint]
 
-    for length in range(len(matrix)-1, 1, -1):
-        print("Length: " + str(length))
+            matrix[startPoint - startPoint + layer][startPoint] = matrix[startPoint][length]
+            matrix[startPoint][length] = matrix[length][length - startPoint + layer]
+            matrix[length][length - startPoint + layer] = matrix[length - startPoint + layer][startPoint - startPoint + layer]
+            matrix[length - startPoint + layer][startPoint - startPoint + layer] = save
         
-        if length != 1 and moduloOne:
-            for start in range(length):
-                save = matrix[start-start][start]
-
-                matrix[start-start][start] = matrix[start][length]
-                matrix[start][length] = matrix[length][length-start]
-                matrix[length][length-start] = matrix[length-start][start-start]
-                matrix[length-start][start-start] = save
+        layer += 1
 
     return matrix
 
 if __name__ == "__main__":
-    matrix = [
-        [1,2,3],
-        [8,9,4],
-        [7,6,5]
+    matrices = [
+        [
+            [1,2,3],
+            [8,9,4],
+            [7,6,5]
+        ],
+        [
+            [1, 2, 3, 4],
+            [12,13,14,5],
+            [11,16,15,6],
+            [10,9, 8, 7]
+        ],
+        [
+            [1, 2, 3, 4, 5],
+            [16, 17, 18, 19, 6],
+            [15, 24, 25, 20, 7],
+            [14, 23, 22, 21, 8],
+            [13, 12, 11, 10, 9]
+        ],
     ]
 
-    # matrix = [
-    #     [1, 2, 3, 4],
-    #     [12,13,14,5],
-    #     [11,16,15,6],
-    #     [10,9, 8, 7]
-    # ]
+    number = 1
+    
+    for matrix in matrices:
+        print("Matrix %i:" % (number))
+        printMatrix(matrix)
 
-    printMatrix(matrix)
+        matrix = rotateMatrixLeft(matrix)
 
-    matrix = rotateMatrix(matrix)
-
-    printMatrix(matrix)
+        printMatrix(matrix)
+        number +=1
